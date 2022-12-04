2419345f16298147f5bedb7171da1bcdb041e121
johntalton <johntalton@gmail.com>
2022-12-04 10:02:15 -0500

    init script exploration

--- src/index.ts
+++ src/index.ts
@@ -20,30 +20,102 @@ import { I2CAddressedBus, I2CBufferSource } from '@johntalton/and-other-delights
 // }
 
 async function writeCommand(addressedDev: I2CAddressedBus, bufferSource: I2CBufferSource) {
-  await addressedDev.i2cWrite(Uint8Array.from([ 0x00 ]))
-
-	await addressedDev.i2cWrite(bufferSource)
+  // await addressedDev.i2cWrite(Uint8Array.from([ 0x00 ]))
+	return await addressedDev.i2cWrite(bufferSource)
 }
 
 async function init(addressedDev: I2CAddressedBus) {
-	// console.log('init')
-	await writeCommand(addressedDev, Uint8Array.from([ 0xAE, 0x00 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0x20, 0x01, 0x00 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0x81, 0x01, 0xcf ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xA1, 0x00 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xA6, 0x00 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xc8, 0x00 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xA8, 0x01, 0x3f ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xd5, 0x01, 0x80 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xd9, 0x01, 0xf1 ]))
-  await writeCommand(addressedDev, Uint8Array.from([ 0xda, 0x01, 0x12 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xdb, 0x01, 0x40 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0x8d, 0x01, 0x14 ]))
-	await writeCommand(addressedDev, Uint8Array.from([ 0xAF, 0x00 ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xAE ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xAF ]))
+	// const result = await addressedDev.i2cRead(1)
+	// const r = new Uint8Array(result)
+	// if(r) {}
+	// return
+	// console.log('init', result)
+
+	await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xAE ])) // display off
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0x20, 0x01, 0x00 ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA1, 0x00 ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA6, 0x00 ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xc8, 0x00 ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA8, 0x01, 0x3f ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xd9, 0x01, 0xf1 ]))
+  // await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xda, 0x01, 0x12 ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xdb, 0x01, 0x40 ]))
+
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xd5, 0x80 ])) // clock divider
+	await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA8, 64 - 1 ])) // multiplexer
+
+	await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xDA, 0x32 ])) // coms pin
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xD3, 0x00 ])) // offset
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0x40, 0x00 ])) // start line
+	await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0x20, 0x00 ])) // mode
+
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA1 ])) // left to right
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0x22, 0, 7 ]))
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xB7 ]))
+
+
+
+	await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0x8d, 0x14 ])) // enable charge pump
+
+	await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0x81, 0x01 ])) // contrast
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA4 ]))  //display ram
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA5 ]))  //display fullon
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA6 ])) // display non invert
+	// await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xA7 ])) // display invert
+	await writeCommand(addressedDev, Uint8Array.from([ 0x00, 0xAF ])) // display on
 
 	// console.log('init done')
 
 	//await addressedDev.i2cbus.driver.stop()
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+	await addressedDev.i2cWrite(Uint8Array.from([ 0x40, ...(new Array(32).fill(0x0f))  ]))
+
+
+	for(let i = 0; i < (128/16 * 64/8) ; i +=1) {
+		const n = () => Math.trunc(Math.random() * 255)
+		await addressedDev.i2cWrite(Uint8Array.from([ 0x40, n(), n(), n(), n(), n(), n(), n(), n(), n(), n(), n(), n(), n(), n(), n(), n()  ]))
+	}
+
 }
 
 
@@ -79,7 +151,8 @@ export class SSD1306 {
 			// setDisplayOn: () => setDisplayOn(this.#addresssedBus),
 			// setDisplayOff: () => setDisplayOff(this.#addresssedBus),
 
-			init: () => init(this.#addresssedBus)
+			init: () => init(this.#addresssedBus),
+			setPixel: (x,y,on) => {}
 		}
 	}
 }
